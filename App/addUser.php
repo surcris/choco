05a74ca20f21a6f106c557f22bcade3e4addd6b1
surcris <sympta113@gmail.com>
2023-03-16 17:10:14 +0100

    ajout user a la base de donnée

--- App/addUser.php
+++ App/addUser.php
@@ -1,3 +1,118 @@
 <?php
-    //logique PHP
+    include './utils/connectBdd.php';
+
+    //tester si le formulaire est submit
+    if (isset($_POST['send'])) {
+       // and !empty($_POST['prenom']) and !empty($_POST['email']) and !empty($_POST['mdp']) AND !empty($_POST['fichier'])
+        //tester si les champs sont remplis
+        if (!empty($_POST['nom']) )
+        {
+            if (!empty($_POST['prenom'])) {
+                
+                if (!empty($_POST['email'])) {
+                    # code...
+                    if (!empty($_POST['mdp'])) {
+                        # code...
+                        if ($_FILES['fichier']['tmp_name']) {
+                            
+                            $nom = $_POST['nom'];
+                            $prenom = $_POST['prenom'];
+                            $email = $_POST['email'];
+                            $mdp = $_POST['mdp'];
+                            $fichier = $_FILES['fichier'];
+                            $destination = '../Public/asset/images/';
+                            if(getUserByEmail($bdd, $email)){
+                                echo "Cette utilisateur existe déjà.";
+                            }else{
+                                //ajouter en BDD
+                                addUser($bdd, $nom, $prenom, $email,$mdp,$fichier);
+                                //afficher une confirmation d'ajout
+                                move_uploaded_file($_FILES['fichier']['tmp_name'], $destination.$_FILES['fichier']['name']);
+                                echo $nom . " " . $prenom . " " . $email." a été ajouter";
+                            }
+                            
+                            
+                            
+                        }else {
+                            $nom = $_POST['nom'];
+                            $prenom = $_POST['prenom'];
+                            $email = $_POST['email'];
+                            $mdp = $_POST['mdp'];
+                            $fichier = "";
+
+                            if(getUserByEmail($bdd, $email)){
+                                echo "Cette utilisateur existe déjà.";
+                            }else{
+                                //ajouter en BDD
+                                addUser($bdd, $nom, $prenom, $email,$mdp,$fichier);
+                                //afficher une confirmation d'ajout
+                                move_uploaded_file($_FILES['fichier']['tmp_name'], $destination.$_FILES['fichier']['name']);
+                                echo $nom . " " . $prenom . " " . $email." a été ajouter";
+                            }
+                            
+                            
+                        }
+                    }else {
+                        echo 'Veuillez remplir mdp';
+                    }
+                }else {
+                    echo 'Veuillez remplir email';
+                }
+
+            }else {
+                echo 'Veuillez remplir prenom';
+            }
+            //stocker le contenu du formulaire
+            
+        } else {
+            echo 'Veuillez remplir nom';
+        }
+    }
+
+    function addUser($bdd, $nom, $prenom, $email,$mdp,$fichier){
+        try{
+            //récupération des paramètres
+            $l_nom = $nom;
+            $l_prenom = $prenom;
+            $l_email = $email;
+            $l_mdp = password_hash($mdp, PASSWORD_DEFAULT);
+            if ($fichier == "") {
+                $l_fichier = '../Public/asset/images/default.png';
+            }else{
+                $l_fichier = $fichier;
+            }
+            $l_role = 1;
+            //préparation de la requête
+            $req2 = $bdd->prepare('INSERT INTO Utilisateur(nom_utilisateur, prenom_utilisateur, mail_utilisateur, password_utilisateur,image_utilisateur,id_roles) VALUES
+            (?, ?, ?, ?, ?, ?)');
+            //affectation des variables
+            $req2->bindParam(1, $l_nom, PDO::PARAM_STR);
+            $req2->bindParam(2, $l_prenom, PDO::PARAM_STR);
+            $req2->bindParam(3, $l_email, PDO::PARAM_STR);
+            $req2->bindParam(4, $l_mdp, PDO::PARAM_STR);
+            $req2->bindParam(5, $l_fichier, PDO::PARAM_STR);
+            $req2->bindParam(6, $l_role, PDO::PARAM_STR);
+            //exécution de la requête
+            $req2->execute();
+        }
+        catch(Exception $e){
+            die('Error: '.$e->getMessage());
+        }
+    }
+
+    function getUserByEmail($bdd, $email){
+        try {
+            //récupérer les paramètres
+            $l_email = $email;
+            $req = $bdd->prepare('SELECT mail_utilisateur FROM utilisateur WHERE mail_utilisateur = ?');
+            $req->bindParam(1, $l_email, PDO::PARAM_STR);
+            $req->execute();
+            //stocker les différents enregistrements dans un tableau associatif
+            $data = $req->fetchAll(PDO::FETCH_ASSOC);
+            return $data;
+        } 
+        catch (Exception $e) {
+            die('Erreur : '.$e->getMessage());
+        }
+    }
 ?>
--- App/vue/view_add_user.php
+++ App/vue/view_add_user.php
@@ -1,14 +1,41 @@
 <!-- partie affichage HTML -->
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../Public/asset/style/main.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../Public/asset/style/index.css">
     <script src="../../Public/asset/script/script.js" defer></script>
     <title>Inscription</title>
 </head>
+
 <body>
-    
+    <div class="login">
+        <div class="form-group">
+            <form action="../addUser.php" method="post" enctype="multipart/form-data">
+                <div class="form-group text-center">
+                    <label>Nom</label>
+                    <input type="text" name="nom" pattern="^[a-zA-Z0-9]+$" class="form-control  my-2"  placeholder="Nom">
+                    <label>Prénom</label>
+                    <input type="text" name="prenom" pattern="^[a-zA-Z0-9]+$" class="form-control  my-2"  placeholder="Prénom">
+                    <label>Email</label>
+                    <input type="text" name="email"  class="form-control  my-2"  placeholder="Email">
+                    <label>Mot de passe</label>
+                    <input type="password" name="mdp" class="form-control  my-2"  placeholder="Mot de passe">
+                    <label>Image</label>
+                    <input type="file" name="fichier" class="form-control  my-2" >
+                </div>
+
+                <div class="col-md-12 text-center">
+                    <button type="submit" class="btn btn-primary my-2" name="send" value="Valider">Valider</button>
+                </div>
+                
+            </form>
+        </div>
+    </div>
 </body>
+
 </html>
