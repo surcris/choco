33f70287e94e6cd8140939afb20e68f93ce79a38
surcris <sympta113@gmail.com>
2023-03-28 14:53:49 +0200

    page connexion

---
+++ App/controller/controllerConnection.php
@@ -0,0 +1,85 @@
+<?php
+include '../utils/connectBdd.php';
+include '../model/utilisateur.php';
+include '../manager/ManagerUtilisateur.php';
+
+
+if (isset($_POST['send'])) {
+    $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+    if (!empty($_POST['nom']) ){
+
+        if (!empty($_POST['prenom'])) {
+            
+            if (!empty($_POST['email']) && $email !== false) {
+                
+                if (!empty($_POST['mdp'])) {
+                    
+                    if ($_FILES['fichier']['tmp_name']) {
+                        
+                        $nom = htmlEntities(strip_tags($_POST['nom']), ENT_QUOTES);
+                        $prenom = htmlEntities(strip_tags($_POST['prenom']), ENT_QUOTES);
+                        $mdp = htmlEntities(strip_tags($_POST['mdp']), ENT_QUOTES);
+                        $fichier = $_FILES['fichier'];
+                        $message = $nom . " " . $prenom . " " . $email." a été ajouter";
+                        //initialisation de l'utilisateur
+                        $user = new ManagerUtilisateur($nom,$prenom,$email,$mdp);
+                        
+
+                        $destination = '../../Public/asset/images/';
+                        $user->setImage($destination.$_FILES['fichier']['name']);
+                        if($user->getUserByMail()){
+                            echo "Cette utilisateur existe déjà.";
+                        }else{
+                            
+                            //ajouter en BDD
+                            $user->insertUser();
+                            //addUser($bddChocoblast->getBdd(), $nom, $prenom, $email,$mdp,$fichier);
+                            //import de l'image
+                            move_uploaded_file($_FILES['fichier']['tmp_name'], $destination.$_FILES['fichier']['name']);
+                            //afficher une confirmation d'ajout
+                            $message = $nom . " " . $prenom . " " . $email." a été ajouter";
+                        }
+                        
+                    }else {
+                        
+                        $nom = $_POST['nom'];
+                        $prenom = $_POST['prenom'];
+                        $email = $_POST['email'];
+                        $mdp = $_POST['mdp'];
+                        //initialisation de l'utilisateur
+                        $user = new ManagerUtilisateur($nom,$prenom,$email,$mdp);
+                        if($user->getUserByMail()){
+                            echo "Cette utilisateur existe déjà.";
+                        }else{
+                            
+                            //ajouter en BDD
+                            $user->insertUser();
+                            //addUser($bddChocoblast->getBdd(), $nom, $prenom, $email,$mdp,$fichier);
+                            //afficher une confirmation d'ajout
+                            $message = $nom . " " . $prenom . " " . $email." a été ajouter";
+                        }
+                        
+                        
+                    }
+
+                }else {
+                    $message = 'Veuillez remplir mdp';
+                }
+
+            }else {
+                $message = 'Veuillez remplir email';
+            }
+
+        }else {
+            $message = 'Veuillez remplir prenom';
+        }
+        
+    } else {
+        $message = 'Veuillez remplir nom';
+    }
+}
+
+include '../vue/header.php';
+include '../vue/view_connection.php';
+include '../vue/footer.php';
+?>
--- App/vue/view_connection.php
+++ App/vue/view_connection.php
@@ -1,22 +1,17 @@
-<!-- partie affichage HTML -->
 <?php 
     include '../vue/header.php';
     include '../vue/footer.php'
 ?>
-    <div class="login">
+
+<div class="login">
         <div class="form-group">
             <form action="#" method="post" enctype="multipart/form-data">
                 <div class="form-group text-center">
-                    <label>Nom</label>
-                    <input type="text" name="nom" pattern="^[a-zA-Z0-9]+$" class="form-control  my-2"  placeholder="Nom">
-                    <label>Prénom</label>
-                    <input type="text" name="prenom" pattern="^[a-zA-Z0-9]+$" class="form-control  my-2"  placeholder="Prénom">
+                    
                     <label>Email</label>
                     <input type="text" name="email" class="form-control  my-2"  placeholder="Email">
                     <label>Mot de passe</label>
                     <input type="password" name="mdp" class="form-control  my-2"  placeholder="Mot de passe">
-                    <label>Image</label>
-                    <input type="file" name="fichier" class="form-control  my-2" >
                     <!-- <div><?php echo $message?></div> -->
                 </div>
 
@@ -24,10 +19,9 @@
                     <button type="submit" class="btn btn-primary my-2" name="send" value="Valider">Valider</button>
                 </div>
                 <div class="col-md-12 text-center">
-                    <a href="../controller/controllerConnection.php" title="About Us"><small class="form-text text-muted text-center">Connexion</small></a>
+                    <a href="../controller/controllerAddUser.php" title="About Us"><small class="form-text text-muted text-center">Inscription</small></a>
                 </div>
                 
             </form>
         </div>
     </div>
-
